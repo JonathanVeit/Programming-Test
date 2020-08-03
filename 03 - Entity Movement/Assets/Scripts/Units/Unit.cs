3749306ba730984ce9bda8d486700778d0f027da
Jonathan Veit <Official.jonathanveit@gmail.com>
2020-08-03 23:18:16 +0200

    [changed] Unity intern stuff

---
+++ 03 - Entity Movement/Assets/Scripts/Units/Unit.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using Unit.Movement;
+using UnityEditor;
+using System.Security.Cryptography;
+using UnityEditor.SceneManagement;
+using System;
+
+namespace Unit
+{
+    public class Unit : MonoBehaviour
+    {
+        [Header("Unit Settings")]
+        [SerializeField] UnitMovementTypes movementType = UnitMovementTypes.Walk;
+        [SerializeField] MovingUnit currentMovement;
+
+        UnitMovementTypes currentType;
+
+        void Awake()
+        {
+            currentType = movementType;
+
+            if (!currentMovement)
+                SwitchMovement(movementType);
+        }
+
+        void SwitchMovement(UnitMovementTypes type)
+        {
+            MovingUnit newMovement = (MovingUnit) this.gameObject.AddComponent (UnitMovementMapping.GetMovementOfType(type));
+
+            if (currentMovement)
+                newMovement.Copy(currentMovement);
+            
+            Destroy(currentMovement);
+            currentMovement = newMovement;
+
+            currentType = type;
+        }
+
+        void OnValidate()
+        {
+            if (currentType != movementType && Application.isPlaying)
+                SwitchMovement(movementType);
+        }
+    }
+}
